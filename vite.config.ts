80de075d68706ea841b125941f7dcb1a1c9454a5
Arturs Jansons <a.jansons@gmail.com>
2025-03-21 22:01:39 +0200

    chore: bump tailwind
    Update Tailwind

--- vite.config.ts
+++ vite.config.ts
@@ -3,10 +3,11 @@ import react from "@vitejs/plugin-react";
 import { fileURLToPath } from "url";
 import { defineConfig } from "vite";
 import svgr from "vite-plugin-svgr";
+import tailwindcss from "@tailwindcss/vite";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [legacy(), react(), svgr()],
+  plugins: [legacy(), react(), svgr(), tailwindcss(),],
   resolve: {
     alias: {
       // for TypeScript path alias import like : @/x/y/z
